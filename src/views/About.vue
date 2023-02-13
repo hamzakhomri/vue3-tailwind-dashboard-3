d160d20cb2802381f406b71ebe7533297f2c1f33
hamzakhomri <hamza.khomri36@gmail.com>
2023-02-13 19:27:06 +0100

    all imported and worked

---
+++ src/views/About.vue
@@ -0,0 +1,14 @@
+<template id="">
+  <div class="about">
+    This is Dasboard
+  </div>
+</template>
+
+<script>
+</script>
+
+<style>
+.about{
+  background-color: aqua;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,14 @@
+<template id="">
+<div class="bg-gray-700">
+  <div class="home">
+    Root 1
+  </div>
+  <div class="home">
+    Root 2
+  </div>
+</div>
+</template>
+
+<style>
+
+</style>
