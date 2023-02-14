b1ddd793614890b723899106a4dba0fb52e9e4c0
hamzakhomri <hamza.khomri36@gmail.com>
2023-02-14 17:15:38 +0100

    button side bar

--- src/views/master/Dashboard.vue
+++ src/views/master/Dashboard.vue
@@ -20,11 +20,40 @@
                             <i class="bi bi-house-door-fill"></i>
                             <span class="text-[15px] ml-4 text-gray-200 font-bold">Home</span>
                         </div>
-                        <div class="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-blue-600 text-white"></div>
                     </router-link>
-                
                 </div>
+
+                <div class="px-[20px] flex flex-col">
+                    <router-link to="/home">
+                        <div class="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-blue-600 text-white">
+                            <svg class="w-6 h-6 dark:text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4"></path></svg>
+                            <span class="text-[15px] ml-4 text-gray-200 font-bold">Profile</span>
+                        </div>
+                    </router-link>
+                </div>
+
+                <div class="px-[20px] flex flex-col">
+                    <router-link to="/home">
+                        <div class="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-blue-600 text-white">
+                            <svg class="w-6 h-6 dark:text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z"></path></svg>
+                            <span class="text-[15px] ml-4 text-gray-200 font-bold">Messages</span>
+                        </div>
+                    </router-link>
+                </div>
+
+                <div class="px-[20px] flex flex-col">
+                    <router-link to="/home">
+                        <div class="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-blue-600 text-white">
+                            <svg class="w-6 h-6 dark:text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 13l-3 3m0 0l-3-3m3 3V8m0 13a9 9 0 110-18 9 9 0 010 18z"></path></svg>
+                            <span class="text-[15px] ml-4 text-gray-200 font-bold">Download</span>
+                        </div>
+                    </router-link>
+                </div>
+   
+
             </div>
+
+            
 
         </div>
                 <!-- END SIDE BAR -->
