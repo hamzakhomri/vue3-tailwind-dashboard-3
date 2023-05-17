0bf65f8763a7f96dcc8e43929180b11e2290a8ee
hamzakhomri <hamza.khomri36@gmail.com>
2023-05-17 15:21:25 +0100

    idont now if SortbyDate Work or note

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -110,7 +110,7 @@
       <th scope="col" class="py-3 px-6">
         <div class="flex items-center">
           Date Modificatin
-          <a href="#"><svg xmlns="http://www.w3.org/2000/svg" class="ml-1 w-3 h-3" aria-hidden="true" fill="currentColor" viewBox="0 0 320 512">
+          <a @click="sortbymodifiedProductCategory"><svg xmlns="http://www.w3.org/2000/svg" class="ml-1 w-3 h-3" aria-hidden="true" fill="currentColor" viewBox="0 0 320 512">
               <path d="M27.66 224h264.7c24.6 0 36.89-29.78 19.54-47.12l-132.3-136.8c-5.406-5.406-12.47-8.107-19.53-8.107c-7.055 0-14.09 2.701-19.45 8.107L8.119 176.9C-9.229 194.2 3.055 224 27.66 224zM292.3 288H27.66c-24.6 0-36.89 29.77-19.54 47.12l132.5 136.8C145.9 477.3 152.1 480 160 480c7.053 0 14.12-2.703 19.53-8.109l132.3-136.8C329.2 317.8 316.9 288 292.3 288z"></path>
             </svg></a>
         </div>
@@ -136,7 +136,7 @@
         {{ category.createdProductCategory }}
       </td>
       <td class="py-4 px-6">
-        {{ category.createdProductCategory }}
+        {{ category.modifiedProductCategory }}
       </td>
 
       <td class="py-4 px-6 text-right">
@@ -166,7 +166,8 @@ export default {
       ProductCategory: [],
       sortById: 'asc',
       sortByName: 'asc',
-      sortBycreatedProductCategory:'asc'
+      sortBycreatedProductCategory:'asc',
+      sortbymodifiedProductCategory:'asc',
     };
   },
   computed: {
@@ -188,47 +189,59 @@ export default {
   },
   methods: {
 //=========== OREDER TABLE==================================
+    sortbymodifiedProductCategory(){
+      this.sortByDate = this.sortByDate === 'asc' ? 'desc' : 'asc';
+        this.ProductCategory.sort((a, b) => {
+          const dateA = new Date(a.modifiedProductCategory);
+          const dateB = new Date(b.modifiedProductCategory);
 
-sortByDateCreation() {
-    this.sortByDate = this.sortByDate === 'asc' ? 'desc' : 'asc';
-    this.ProductCategory.sort((a, b) => {
-      const dateA = new Date(a.createdProductCategory);
-      const dateB = new Date(b.createdProductCategory);
+          if (this.sortByDate === 'asc') {
+            return dateA - dateB;
+          } else {
+            return dateB - dateA;
+          }
+        });
+    },
+    sortByDateCreation() {
+        this.sortByDate = this.sortByDate === 'asc' ? 'desc' : 'asc';
+        this.ProductCategory.sort((a, b) => {
+          const dateA = new Date(a.createdProductCategory);
+          const dateB = new Date(b.createdProductCategory);
 
-      if (this.sortByDate === 'asc') {
-        return dateA - dateB;
-      } else {
-        return dateB - dateA;
-      }
-    });
-  },
-sortByNameCategory() {
-    this.sortByName = this.sortByName === 'asc' ? 'desc' : 'asc';
-    this.ProductCategory.sort((a, b) => {
-      const nameA = a.nameProductCategory.toUpperCase();
-      const nameB = b.nameProductCategory.toUpperCase();
+          if (this.sortByDate === 'asc') {
+            return dateA - dateB;
+          } else {
+            return dateB - dateA;
+          }
+        });
+      },
+    sortByNameCategory() {
+        this.sortByName = this.sortByName === 'asc' ? 'desc' : 'asc';
+        this.ProductCategory.sort((a, b) => {
+          const nameA = a.nameProductCategory.toUpperCase();
+          const nameB = b.nameProductCategory.toUpperCase();
 
-      if (this.sortByName === 'asc') {
-        if (nameA < nameB) return -1;
-        if (nameA > nameB) return 1;
-      } else {
-        if (nameA > nameB) return -1;
-        if (nameA < nameB) return 1;
-      }
+          if (this.sortByName === 'asc') {
+            if (nameA < nameB) return -1;
+            if (nameA > nameB) return 1;
+          } else {
+            if (nameA > nameB) return -1;
+            if (nameA < nameB) return 1;
+          }
 
-      return 0;
-    });
-  },
-sortByIdCategory() {
-    this.sortBy = this.sortBy === 'asc' ? 'desc' : 'asc';
-    this.ProductCategory.sort((a, b) => {
-      if (this.sortBy === 'asc') {
-        return a.idProductCategory - b.idProductCategory;
-      } else {
-        return b.idProductCategory - a.idProductCategory;
-      }
-    });
-  },
+          return 0;
+        });
+      },
+    sortByIdCategory() {
+        this.sortBy = this.sortBy === 'asc' ? 'desc' : 'asc';
+        this.ProductCategory.sort((a, b) => {
+          if (this.sortBy === 'asc') {
+            return a.idProductCategory - b.idProductCategory;
+          } else {
+            return b.idProductCategory - a.idProductCategory;
+          }
+        });
+      },
   //=========== END OREDER TABLE==================================
     // Delete CATEGORY TO SPRINGBOOT
   deleteProductCategory(idProductCategory) {
