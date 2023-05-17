8b34e8c3eec062f8f78ca0dd999ea596c5ab1db3
hamzakhomri <hamza.khomri36@gmail.com>
2023-05-17 15:02:58 +0100

    sortByIdCategory

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -80,9 +80,9 @@
   <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
     <tr>
       <th scope="col" class="py-3 px-6">
-        <div class="flex items-center">
-         ID
-          <a href="#">
+        <div class="flex items-center" >
+          ID
+          <a @click="sortByIdCategory">
             <svg xmlns="http://www.w3.org/2000/svg" class="ml-1 w-3 h-3" aria-hidden="true" fill="currentColor" viewBox="0 0 320 512">
               <path d="M27.66 224h264.7c24.6 0 36.89-29.78 19.54-47.12l-132.3-136.8c-5.406-5.406-12.47-8.107-19.53-8.107c-7.055 0-14.09 2.701-19.45 8.107L8.119 176.9C-9.229 194.2 3.055 224 27.66 224zM292.3 288H27.66c-24.6 0-36.89 29.77-19.54 47.12l132.5 136.8C145.9 477.3 152.1 480 160 480c7.053 0 14.12-2.703 19.53-8.109l132.3-136.8C329.2 317.8 316.9 288 292.3 288z"></path>
             </svg>
@@ -163,7 +163,8 @@ export default {
       newItem: '',
       CategoryName: '',
       searchCategory: '',
-      ProductCategory: []
+      ProductCategory: [],
+      sortBy: 'asc'
     };
   },
   computed: {
@@ -184,7 +185,18 @@ export default {
       });
   },
   methods: {
-
+//=========== OREDER TABLE==================================
+    sortByIdCategory() {
+    this.sortBy = this.sortBy === 'asc' ? 'desc' : 'asc';
+    this.ProductCategory.sort((a, b) => {
+      if (this.sortBy === 'asc') {
+        return a.idProductCategory - b.idProductCategory;
+      } else {
+        return b.idProductCategory - a.idProductCategory;
+      }
+    });
+  },
+  //=========== END OREDER TABLE==================================
     // Delete CATEGORY TO SPRINGBOOT
   deleteProductCategory(idProductCategory) {
     axios.delete(`http://localhost:8080/productcategory/${idProductCategory}`)
