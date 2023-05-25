af7c3f0f660bc0dc0df801a398253bdc017f07e3
hamzakhomri <hamza.khomri36@gmail.com>
2023-05-25 17:07:41 +0100

    Search Product { Price && QTE } DOnt work

--- src/views/Products.vue
+++ src/views/Products.vue
@@ -146,7 +146,7 @@
               <path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path>
             </svg>
           </div>
-          <input type="text" id="searchCategory" v-model="searchProductQte" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-[100%] pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Price ..." required>
+          <input type="number" id="searchCategory" v-model="searchProductQte" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-[100%] pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Price ..." required>
         </div>
       </div>
 
@@ -328,6 +328,7 @@
   </template>
   <script>
   import axios from 'axios';
+import { parse } from 'postcss';
   
   export default {
 data() {
@@ -370,7 +371,7 @@ computed: {
             
             if (this.searchProductQte !== '') {
                 return this.Product.filter(product =>
-                product.qteProducts === this.searchProductQte
+                parseInt(product.qteProducts) ===parseInt(this.searchProductQte)
                 );
               }
 
