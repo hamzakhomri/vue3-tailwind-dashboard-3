41117f48d32b87ddace52919558e47aa855b3fe5
hamzakhomri <hamza.khomri36@gmail.com>
2023-05-25 16:18:02 +0100

    Search Product Name

--- src/views/Products.vue
+++ src/views/Products.vue
@@ -103,7 +103,7 @@
               <path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path>
             </svg>
           </div>
-          <input type="number" id="searchCategory" v-model="searchCategoryID" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-[100%] pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="ID ..." required>
+          <input type="number" id="searchCategory" v-model="searchProductId" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-[100%] pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="ID ..." required>
         </div>
       </div>
   
@@ -317,7 +317,7 @@ data() {
     
   
         searchProductname: '',
-        searchCategoryID:'',
+        searchProductId:'',
         searchDateModification:'',
         searchDateCreation: '',
   
@@ -328,15 +328,15 @@ computed: {
 
           //========= Search
           ProductCategory() {
-              if (this.searchCategoryID !== '') {
-                return this.ProductCategory.filter(category =>
-                  category.idProducts === this.searchCategoryID
+              if (this.searchProductId !== '') {
+                return this.ProductCategory.filter(product =>
+                product.idProducts === this.searchProductId
                 );
               }
               
               if (this.searchProductname !== '') {
-                return this.ProductCategory.filter(category =>
-                  category.nameProductCategory.includes(this.searchProductname)
+                return this.ProductCategory.filter(product =>
+                product.nameProducts.includes(this.searchProductname)
                 );
               }
               
