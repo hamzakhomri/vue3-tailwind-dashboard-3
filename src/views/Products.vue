80e83a92870a6f777c8196847fa470fc80183214
hamzakhomri <hamza.khomri36@gmail.com>
2023-05-25 16:34:18 +0100

    search by priceProducts dont work

--- src/views/Products.vue
+++ src/views/Products.vue
@@ -117,6 +117,20 @@
           <input type="text" id="searchCategory" v-model="searchProductname" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-[100%] pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Name ..." required>
         </div>
       </div>
+
+
+      <div class="flex items-center w-full mb-4">
+        <div class="relative w-full ">
+          <div class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none ">
+            <svg aria-hidden="true" class="w-5 h-auto text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+              <path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path>
+            </svg>
+          </div>
+          <input type="text" id="searchCategory" v-model="searchProductPrice" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-[100%] pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Price ..." required>
+        </div>
+      </div>
+
+      
   
       <div class="flex items-center w-full mb-4">
         <div class="relative w-full ">
@@ -315,19 +329,23 @@ data() {
         sortbyqtes:'asc',
         sortbynames:'asc',
     
-  
-        searchProductname: '',
         searchProductId:'',
+        searchProductname: '',
+        searchProductPrice:'',
         searchDateModification:'',
         searchDateCreation: '',
-  
       };
     },
-    
+
 computed: {
 
           //========= Search
           ProductCategory() {
+            if (this.searchProductPrice !== '') {
+                return this.ProductCategory.filter(product =>
+                product.priceProducts === this.searchProductPrice
+                );
+              }
               if (this.searchProductId !== '') {
                 return this.ProductCategory.filter(product =>
                 product.idProducts === this.searchProductId
@@ -339,13 +357,12 @@ computed: {
                 product.nameProducts.includes(this.searchProductname)
                 );
               }
-              
               if (this.searchDateCreation!=='') {
               const dateParts = this.searchDateCreation.split('-');
               const searchDates = `${dateParts[2]}-${dateParts[1]}-${dateParts[0]}`;
   
-              return this.ProductCategory.filter(category => 
-                category.createdProductCategory === searchDates
+              return this.ProductCategory.filter(product => 
+              product.createdatProduct === searchDates
               );
             }
             if (this.searchDateModification!=='') {
@@ -353,7 +370,7 @@ computed: {
               const searchDates = `${dateParts[2]}-${dateParts[1]}-${dateParts[0]}`;
   
               return this.ProductCategory.filter(category => 
-                category.modifiedProductCategory === searchDates
+                category.modifiedatProduct === searchDates
               );
             }
               
