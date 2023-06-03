292f3bc0ebda049f8415fa0e025195eccb19fd2b
hamzakhomri <hamza.khomri36@gmail.com>
2023-06-03 13:55:58 +0100

    get category and assignign product+category

--- src/views/Products.vue
+++ src/views/Products.vue
@@ -399,6 +399,16 @@
           </div>
         </th>
         <th scope="col" class="py-3 px-6">
+          <div class="cursor-pointer flex items-center">
+            Categories
+            <a>
+              <svg xmlns="http://www.w3.org/2000/svg" class="ml-1 w-3 h-3" aria-hidden="true" fill="currentColor" viewBox="0 0 320 512">
+                <path d="M27.66 224h264.7c24.6 0 36.89-29.78 19.54-47.12l-132.3-136.8c-5.406-5.406-12.47-8.107-19.53-8.107c-7.055 0-14.09 2.701-19.45 8.107L8.119 176.9C-9.229 194.2 3.055 224 27.66 224zM292.3 288H27.66c-24.6 0-36.89 29.77-19.54 47.12l132.5 136.8C145.9 477.3 152.1 480 160 480c7.053 0 14.12-2.703 19.53-8.109l132.3-136.8C329.2 317.8 316.9 288 292.3 288z"></path>
+              </svg>
+            </a>
+          </div>
+        </th>
+        <th scope="col" class="py-3 px-6">
           <div @click="sortByPrice" class="cursor-pointer flex items-center">
             Price
             <a>
@@ -444,6 +454,9 @@
         </td>
         <td class="py-4 px-6 ">
           {{ product.nameProducts }}
+        </td>
+        <td class="py-4 px-6">
+          {{ product.productCategory.nameProductCategory }}
         </td>
         <td class="py-4 px-6">
           {{ product.priceProducts }}
