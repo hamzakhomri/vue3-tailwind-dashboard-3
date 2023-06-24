71575c68899c4c647acd95f1ff89a4ff876aadb0
hamzakhomri <hamza.khomri36@gmail.com>
2023-06-24 13:53:16 +0100

    delete picture with confirmation dialgue

--- src/views/Tester.vue
+++ src/views/Tester.vue
@@ -11,10 +11,12 @@
       <li v-for="productpicture in myData" :key="productpicture.idProductPicture" class="flex items-center mb-2">
         <span class="mr-2">{{ productpicture.namePicture }}</span>
         <img :src="getPictureSrc(productpicture.picture)" alt="Product Picture" class="h-40 w-40 rounded" />
+        <button @click="confirmDelete(productpicture.idProductPicture)" class="text-white">Delete</button>
       </li>
     </ul>
   </div>
 </template>
+
 
 
   
@@ -34,53 +36,84 @@
 
 
     methods: {
-GetAll(){
-  axios.get('http://localhost:8080/productpicture').then((response) => {
+
+
+
+ confirmDelete(pictureId) {
+      if (confirm('Are you sure you want to delete this picture?')) {
+        this.deletePicture(pictureId);
+      }
+    },
+    deletePicture(pictureId) {
+      axios.delete(`http://localhost:8080/productpicture/${pictureId}`)
+        .then(response => {
+          // Handle success
+          console.log('Picture deleted successfully!');
+          // Remove the deleted picture from myData array
+          this.myData = this.myData.filter(productpicture => productpicture.idProductPicture !== pictureId);
+        })
+        .catch(error => {
+          // Handle error
+          console.error('Failed to delete picture:', error);
+        });
+    },
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+      GetAll(){
+      axios.get('http://localhost:8080/productpicture').then((response) => {
           this.myData = response.data;
         })
         .catch((error) => {
           console.error(error);
         });
-},
+    },
 
-   
       onPictureChange(event) {
       this.pictureFile = event.target.files[0];
     },
-    uploadPicture() {
-        if (!this.pictureFile) {
-          console.log('No file selected');
-          return;
-        }
 
-        const formData = new FormData();
-        formData.append('file', this.pictureFile);
-      // Use 'file' instead of 'picture'
+      uploadPicture() {
+          if (!this.pictureFile) {
+            console.log('No file selected');
+            return;
+          }
 
-        axios
-          .post('http://localhost:8080/productpicture', formData).then((response) => {
-            console.log(response.data);
-            this.GetAll();
-          })
-          .catch((error) => {
-            if (error.response) {
-              console.log("eroor is :" + error);
-              console.log("this :"+this.pictureFile);
-              console.log(error.response.data);
-              console.log(error.response.status);
-              console.log(error.response.headers);
-            }
-          });
+          const formData = new FormData();
+          formData.append('file', this.pictureFile);
+
+          axios
+            .post('http://localhost:8080/productpicture', formData).then((response) => {
+              console.log(response.data);
+              this.GetAll();
+            })
+            .catch((error) => {
+              if (error.response) {
+                console.log("eroor is :" + error);
+                console.log("this :"+this.pictureFile);
+                console.log(error.response.data);
+                console.log(error.response.status);
+                console.log(error.response.headers);
+              }
+            });
     },
 
-            
-
-          getPictureSrc(picture) {
-            if (picture) {
-              return `data:image/jpeg;base64,${picture}`;
-            }
-            return '';
-          },
+      getPictureSrc(picture) {
+        if (picture) {
+          return `data:image/jpeg;base64,${picture}`;
+        }
+        return '';
+      },
     },
   };
   </script>
