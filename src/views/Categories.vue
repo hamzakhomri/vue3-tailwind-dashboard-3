7b3177e56723acbd7af632da2452655564d8ba39
hamzakhomri <hamza.khomri36@gmail.com>
2023-08-10 13:26:44 +0100

    cleaning front

--- src/views/Categories.vue
+++ src/views/Categories.vue
@@ -208,10 +208,20 @@
               </td>
 
               <td class="py-4 px-6 text-right">
-                <button @click="confirmDelete(category.idProductCategory)">Delete</button>
+                <button @click="confirmDelete(category.idProductCategory)" class="inline-flex items-center px-4 py-2 bg-red-600 hover:bg-red-700 text-white text-sm font-medium rounded-md">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+                </svg>
+                Supprimer
+              </button>
               </td>
               <td class="py-4 px-6 text-right">
-                <button @click="confirmUpdate(category.idProductCategory,category.nameProductCategory)">Modifier</button>
+                <button @click="confirmUpdate(category.idProductCategory,category.nameProductCategory)" class="inline-flex items-center px-4 py-2 bg-green-400 hover:bg-green-500 text-gray-800 text-sm font-medium rounded-md">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 8h14M5 8a2 2 0 110-4h14a2 2 0 110 4M5 8v10a2 2 0 002 2h10a2 2 0 002-2V8m-9 4h4" />
+                </svg>
+                Modifier
+              </button>
               </td>
               <transition name="slide">
             <div v-if="DialogueDelete && categoryIdToDelete===category.idProductCategory" class="flex items-center justify-center absolute top-0 left-0 w-full h-full  bg-red-200 modal text-gray-500 py-4">
